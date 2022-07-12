a6eacb60cefa9e2f209af772d5b868715bfd2150
Javier Bullrich <javier@bullrich.dev>
2022-07-12 21:40:22 +0100

    chore(server): created basic project

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import NodeCache from "node-cache";
+
+const app = express();
+
+const cache = new NodeCache({ stdTTL: 30, checkperiod: 45 });
+
+app.use(cors());
+
+const jsonParser = bodyParser.json();
+
+app.use(jsonParser);
+
+app.use("/api/health", (_, res) => {
+    res.send("Ok");
+});
+
+app.get("/api/cache", (req, res) => {
+    const data = req.query.data as string;
+    if (!data) {
+        return res.status(406);
+    }
+    const cachedResponse = cache.get<string>(data);
+    if (cachedResponse) {
+        return res.send(cachedResponse);
+    } else {
+        cache.set(data, data);
+        return res.send("cached!");
+    }
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`\n Server is running on http://localhost:${port}\n`);
+});
