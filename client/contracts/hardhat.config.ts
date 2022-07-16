27be0fa913d7ed58c61b46a98b109a7a1bef8c1b
Javier Bullrich <javier@bullrich.dev>
2022-07-16 20:13:03 +0200

    chore(contracts): fixed contract directory

--- client/contracts/hardhat.config.ts
+++ client/contracts/hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   typechain: {
-    outDir: "../src/contracts",
+    outDir: "../src/lib/contracts",
     target: "ethers-v5",
   },
 };
