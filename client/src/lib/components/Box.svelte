83a1861c8232622d009985e2404cf8002fb52b5f
Javier Bullrich <javier@bullrich.dev>
2022-07-17 23:03:53 +0200

    feat(client): added page to show box content

---
+++ client/src/lib/components/Box.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { Col, Container, Row } from "sveltestrap";
+    import type { InterplanetaryBox } from "../contracts/contracts/InterplanetaryBox";
+    import { decryptMessage } from "../utils/walletUtils";
+    import DownloadCard from "./Box/DownloadCard.svelte";
+    import UploadCard from "./Box/UploadCard.svelte";
+
+    export let box: InterplanetaryBox;
+    let filesPromise = box.getFiles();
+    let encryptionKey: string;
+    let fileVar: FileList;
+
+    async function getKey(): Promise<string> {
+        if (!encryptionKey) {
+            const key = await box.Key();
+            encryptionKey = await decryptMessage(key);
+        }
+        return encryptionKey;
+    }
+</script>
+
+<Container>
+    {#await filesPromise}
+        <h5>Loading files from the smart contract</h5>
+    {:then files}
+        <Row>
+            <Col>
+                <UploadCard
+                    {box}
+                    {getKey}
+                    onUpload={() => (filesPromise = box.getFiles())}
+                />
+            </Col>
+            {#each files as file}
+                <Col>
+                    <DownloadCard {box} fileName={file} {getKey} />
+                </Col>
+            {/each}
+        </Row>
+    {/await}
+</Container>
