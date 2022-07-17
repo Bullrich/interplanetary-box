04b8214ce7a742507ce03597fc01dddb0d4929e2
Javier Bullrich <javier@bullrich.dev>
2022-07-17 23:02:34 +0200

    feat(client): added code to deploy contract

--- client/src/lib/utils/contract.ts
+++ client/src/lib/utils/contract.ts
@@ -1,13 +1,17 @@
 import type { ethers } from "ethers";
 import { InterplanetaryBox__factory } from "../contracts/factories/contracts/InterplanetaryBox__factory";
+import { connectWallet } from "../stores/wallet";
 import { Chains, changeNetwork } from "./chains";
 
-export const deploy = async (provider: ethers.providers.JsonRpcSigner, key: string) => {
-    await changeNetwork(Chains.MUMBAI);
+export const deploy = async (key: string) => {
+    await changeNetwork(Chains.OPTIMISM_KOVAN);
+
+    const provider = await connectWallet()
+    const signer = provider.getSigner();
 
     // console.log(provider, provider.network);
     // const signer = await provider.getSigner();
-    const factory = new InterplanetaryBox__factory(provider);
+    const factory = new InterplanetaryBox__factory(signer);
     const interplanetaryBox = await factory.deploy(key);
     return interplanetaryBox;
 }
