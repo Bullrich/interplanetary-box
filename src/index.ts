116e826bcff41c20d607c9ef5b89eea3e39577f6
Javier Bullrich <javier@bullrich.dev>
2022-07-18 23:18:47 +0200

    feat(server): added contract address db storage

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import bodyParser from "body-parser";
 import cors from "cors";
 import crypto from "crypto";
 import "dotenv/config";
 import express from "express";
 import fileUpload from "express-fileupload";
-import NodeCache from "node-cache";
 import path from "path";
 import { encrypt } from "./utils/encrypt";
 import { storeFile } from "./utils/ipfs";
 
 const app = express();
-
-const cache = new NodeCache({ stdTTL: 30, checkperiod: 45 });
+const prisma = new PrismaClient();
 
 app.use(cors());
 
@@ -20,8 +19,8 @@ const jsonParser = bodyParser.json();
 app.use(jsonParser);
 
 app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : "/tmp/",
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
     limits: { fileSize: 50 * 1024 * 1024 },
 }));
 
@@ -30,7 +29,7 @@ app.use("/api/health", (_, res) => {
 });
 
 app.post("/api/encrypt", (req, res) => {
-    const data = req.body as {key:string};
+    const data = req.body as { key: string };
 
     const id = crypto.randomUUID();
 
@@ -38,31 +37,46 @@ app.post("/api/encrypt", (req, res) => {
     return res.send(encryptedData);
 });
 
-app.get("/api/cache", (req, res) => {
-    const data = req.query.data as string;
-    if (!data) {
-        return res.status(406);
+app.post("/api/upload", async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send("No files were uploaded.");
     }
-    const cachedResponse = cache.get<string>(data);
-    if (cachedResponse) {
-        return res.send(cachedResponse);
-    } else {
-        cache.set(data, data);
-        return res.send("cached!");
-    }
+
+    const file = req.files.files as fileUpload.UploadedFile;
+
+    const cid = await storeFile(file.tempFilePath);
+
+    return res.send(cid);
 });
 
-app.post("/api/upload", async (req, res) => {
-        if (!req.files || Object.keys(req.files).length === 0) {
-            return res.status(400).send("No files were uploaded.");
+app.post("/api/contract", async (req, res) => {
+    const data = req.body as { address: string, contract: string };
+
+    await prisma.$connect();
+    await prisma.box.create({ data });
+    return res.status(201);
+});
+
+app.get("/api/contract", async (req, res, next) => {
+    const address = req.query.address as string;
+    if (!address) {
+        return res.status(400);
+    }
+    console.log(address);
+    await prisma.$connect();
+    const contract = await prisma.box.findUnique({
+        where: {
+            address
         }
-
-        const file = req.files.files as fileUpload.UploadedFile;
-
-        const cid = await storeFile(file.tempFilePath);
-
-        return res.send(cid);
     });
+    console.log(contract);
+    if (!contract) {
+        res.statusCode = 204;
+        return res.send("Not found");
+        // return next("Not found - 404")
+    }
+    return res.send(contract.contract);
+});
 
 app.use("/", express.static("./client/dist"));
 
